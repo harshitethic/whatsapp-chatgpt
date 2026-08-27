8d70aae8e476f9003cde0d20dd55e763f213642b
Harshit Sharma <66710144+harshitethic@users.noreply.github.com>
2026-08-28 01:42:24 +0530

    chore: remove duplicate configuration test

--- placeholder-config-test.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const assert = require('assert');
-const config = JSON.parse(fs.readFileSync('./accesser.json', 'utf8'));
-assert.strictEqual(config.keyopenai, 'ISI_APIKEY_OPENAI_DISINI');
-console.log('placeholder configuration check passed');
